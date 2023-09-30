f7c457c700669126490372a3e52b8846e1e787f9
xNotDanielx <vdanieltorres@unicesar.edu.co>
2023-09-30 14:24:30 -0500

    first commit

---
+++ Puzzle/Puzzle/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Btn5 = new System.Windows.Forms.Button();
+            this.Btn6 = new System.Windows.Forms.Button();
+            this.Btn7 = new System.Windows.Forms.Button();
+            this.Btn9 = new System.Windows.Forms.Button();
+            this.Btn10 = new System.Windows.Forms.Button();
+            this.Btn11 = new System.Windows.Forms.Button();
+            this.Btn13 = new System.Windows.Forms.Button();
+            this.Btn14 = new System.Windows.Forms.Button();
+            this.Btn15 = new System.Windows.Forms.Button();
+            this.Btn4 = new System.Windows.Forms.Button();
+            this.Btn8 = new System.Windows.Forms.Button();
+            this.Btn12 = new System.Windows.Forms.Button();
+            this.Btn16 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnNuevaPartida = new System.Windows.Forms.Button();
+            this.LblContadorClicks = new System.Windows.Forms.Label();
+            this.TimerContador = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LblTiempo = new System.Windows.Forms.Label();
+            this.BtnPausar = new System.Windows.Forms.Button();
+            this.BtnReanudar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(25, 36);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(65, 57);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "1";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Location = new System.Drawing.Point(96, 36);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(65, 57);
+            this.Btn2.TabIndex = 1;
+            this.Btn2.Text = "2";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Location = new System.Drawing.Point(167, 36);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(65, 57);
+            this.Btn3.TabIndex = 2;
+            this.Btn3.Text = "3";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // Btn5
+            // 
+            this.Btn5.Location = new System.Drawing.Point(25, 99);
+            this.Btn5.Name = "Btn5";
+            this.Btn5.Size = new System.Drawing.Size(65, 57);
+            this.Btn5.TabIndex = 3;
+            this.Btn5.Text = "5";
+            this.Btn5.UseVisualStyleBackColor = true;
+            this.Btn5.Click += new System.EventHandler(this.Btn5_Click);
+            // 
+            // Btn6
+            // 
+            this.Btn6.Location = new System.Drawing.Point(96, 99);
+            this.Btn6.Name = "Btn6";
+            this.Btn6.Size = new System.Drawing.Size(65, 57);
+            this.Btn6.TabIndex = 4;
+            this.Btn6.Text = "6";
+            this.Btn6.UseVisualStyleBackColor = true;
+            this.Btn6.Click += new System.EventHandler(this.Btn6_Click);
+            // 
+            // Btn7
+            // 
+            this.Btn7.Location = new System.Drawing.Point(167, 99);
+            this.Btn7.Name = "Btn7";
+            this.Btn7.Size = new System.Drawing.Size(65, 57);
+            this.Btn7.TabIndex = 5;
+            this.Btn7.Text = "7";
+            this.Btn7.UseVisualStyleBackColor = true;
+            this.Btn7.Click += new System.EventHandler(this.Btn7_Click);
+            // 
+            // Btn9
+            // 
+            this.Btn9.Location = new System.Drawing.Point(25, 162);
+            this.Btn9.Name = "Btn9";
+            this.Btn9.Size = new System.Drawing.Size(65, 57);
+            this.Btn9.TabIndex = 6;
+            this.Btn9.Text = "9";
+            this.Btn9.UseVisualStyleBackColor = true;
+            this.Btn9.Click += new System.EventHandler(this.Btn9_Click);
+            // 
+            // Btn10
+            // 
+            this.Btn10.Location = new System.Drawing.Point(96, 162);
+            this.Btn10.Name = "Btn10";
+            this.Btn10.Size = new System.Drawing.Size(65, 57);
+            this.Btn10.TabIndex = 7;
+            this.Btn10.Text = "10";
+            this.Btn10.UseVisualStyleBackColor = true;
+            this.Btn10.Click += new System.EventHandler(this.Btn10_Click);
+            // 
+            // Btn11
+            // 
+            this.Btn11.Location = new System.Drawing.Point(167, 162);
+            this.Btn11.Name = "Btn11";
+            this.Btn11.Size = new System.Drawing.Size(65, 57);
+            this.Btn11.TabIndex = 8;
+            this.Btn11.Text = "11";
+            this.Btn11.UseVisualStyleBackColor = true;
+            this.Btn11.Click += new System.EventHandler(this.Btn11_Click);
+            // 
+            // Btn13
+            // 
+            this.Btn13.Location = new System.Drawing.Point(25, 225);
+            this.Btn13.Name = "Btn13";
+            this.Btn13.Size = new System.Drawing.Size(65, 57);
+            this.Btn13.TabIndex = 9;
+            this.Btn13.Text = "13";
+            this.Btn13.UseVisualStyleBackColor = true;
+            this.Btn13.Click += new System.EventHandler(this.Btn13_Click);
+            // 
+            // Btn14
+            // 
+            this.Btn14.Location = new System.Drawing.Point(96, 225);
+            this.Btn14.Name = "Btn14";
+            this.Btn14.Size = new System.Drawing.Size(65, 57);
+            this.Btn14.TabIndex = 10;
+            this.Btn14.Text = "14";
+            this.Btn14.UseVisualStyleBackColor = true;
+            this.Btn14.Click += new System.EventHandler(this.Btn14_Click);
+            // 
+            // Btn15
+            // 
+            this.Btn15.Location = new System.Drawing.Point(167, 225);
+            this.Btn15.Name = "Btn15";
+            this.Btn15.Size = new System.Drawing.Size(65, 57);
+            this.Btn15.TabIndex = 11;
+            this.Btn15.Text = "15";
+            this.Btn15.UseVisualStyleBackColor = true;
+            this.Btn15.Click += new System.EventHandler(this.Btn15_Click);
+            // 
+            // Btn4
+            // 
+            this.Btn4.Location = new System.Drawing.Point(238, 36);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.Size = new System.Drawing.Size(65, 57);
+            this.Btn4.TabIndex = 12;
+            this.Btn4.Text = "4";
+            this.Btn4.UseVisualStyleBackColor = true;
+            this.Btn4.Click += new System.EventHandler(this.Btn4_Click);
+            // 
+            // Btn8
+            // 
+            this.Btn8.Location = new System.Drawing.Point(238, 99);
+            this.Btn8.Name = "Btn8";
+            this.Btn8.Size = new System.Drawing.Size(65, 57);
+            this.Btn8.TabIndex = 13;
+            this.Btn8.Text = "8";
+            this.Btn8.UseVisualStyleBackColor = true;
+            this.Btn8.Click += new System.EventHandler(this.Btn8_Click);
+            // 
+            // Btn12
+            // 
+            this.Btn12.Location = new System.Drawing.Point(238, 162);
+            this.Btn12.Name = "Btn12";
+            this.Btn12.Size = new System.Drawing.Size(65, 57);
+            this.Btn12.TabIndex = 14;
+            this.Btn12.Text = "12";
+            this.Btn12.UseVisualStyleBackColor = true;
+            this.Btn12.Click += new System.EventHandler(this.Btn12_Click);
+            // 
+            // Btn16
+            // 
+            this.Btn16.Location = new System.Drawing.Point(238, 225);
+            this.Btn16.Name = "Btn16";
+            this.Btn16.Size = new System.Drawing.Size(65, 57);
+            this.Btn16.TabIndex = 15;
+            this.Btn16.Text = "button16";
+            this.Btn16.UseVisualStyleBackColor = true;
+            this.Btn16.Click += new System.EventHandler(this.Btn16_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Btn1);
+            this.groupBox1.Controls.Add(this.Btn2);
+            this.groupBox1.Controls.Add(this.Btn16);
+            this.groupBox1.Controls.Add(this.Btn3);
+            this.groupBox1.Controls.Add(this.Btn12);
+            this.groupBox1.Controls.Add(this.Btn5);
+            this.groupBox1.Controls.Add(this.Btn8);
+            this.groupBox1.Controls.Add(this.Btn6);
+            this.groupBox1.Controls.Add(this.Btn4);
+            this.groupBox1.Controls.Add(this.Btn7);
+            this.groupBox1.Controls.Add(this.Btn15);
+            this.groupBox1.Controls.Add(this.Btn9);
+            this.groupBox1.Controls.Add(this.Btn14);
+            this.groupBox1.Controls.Add(this.Btn10);
+            this.groupBox1.Controls.Add(this.Btn13);
+            this.groupBox1.Controls.Add(this.Btn11);
+            this.groupBox1.Location = new System.Drawing.Point(50, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 303);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // BtnNuevaPartida
+            // 
+            this.BtnNuevaPartida.Location = new System.Drawing.Point(528, 279);
+            this.BtnNuevaPartida.Name = "BtnNuevaPartida";
+            this.BtnNuevaPartida.Size = new System.Drawing.Size(138, 47);
+            this.BtnNuevaPartida.TabIndex = 17;
+            this.BtnNuevaPartida.Text = "Nueva Partida";
+            this.BtnNuevaPartida.UseVisualStyleBackColor = true;
+            this.BtnNuevaPartida.Click += new System.EventHandler(this.BtnNuevaPartida_Click);
+            // 
+            // LblContadorClicks
+            // 
+            this.LblContadorClicks.AutoSize = true;
+            this.LblContadorClicks.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblContadorClicks.Location = new System.Drawing.Point(190, 35);
+            this.LblContadorClicks.Name = "LblContadorClicks";
+            this.LblContadorClicks.Size = new System.Drawing.Size(57, 16);
+            this.LblContadorClicks.TabIndex = 18;
+            this.LblContadorClicks.Text = "Clicks: 0";
+            // 
+            // TimerContador
+            // 
+            this.TimerContador.Interval = 1000;
+            this.TimerContador.Tick += new System.EventHandler(this.TimerContador_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BtnPausar);
+            this.groupBox2.Controls.Add(this.BtnReanudar);
+            this.groupBox2.Controls.Add(this.LblTiempo);
+            this.groupBox2.Location = new System.Drawing.Point(469, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(247, 100);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tiempo";
+            // 
+            // LblTiempo
+            // 
+            this.LblTiempo.AutoSize = true;
+            this.LblTiempo.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTiempo.Location = new System.Drawing.Point(94, 27);
+            this.LblTiempo.Name = "LblTiempo";
+            this.LblTiempo.Size = new System.Drawing.Size(57, 16);
+            this.LblTiempo.TabIndex = 19;
+            this.LblTiempo.Text = "00:00:00";
+            // 
+            // BtnPausar
+            // 
+            this.BtnPausar.Location = new System.Drawing.Point(137, 66);
+            this.BtnPausar.Name = "BtnPausar";
+            this.BtnPausar.Size = new System.Drawing.Size(86, 28);
+            this.BtnPausar.TabIndex = 20;
+            this.BtnPausar.Text = "Pausar";
+            this.BtnPausar.UseVisualStyleBackColor = true;
+            this.BtnPausar.Click += new System.EventHandler(this.BtnPausar_Click);
+            // 
+            // BtnReanudar
+            // 
+            this.BtnReanudar.Location = new System.Drawing.Point(26, 66);
+            this.BtnReanudar.Name = "BtnReanudar";
+            this.BtnReanudar.Size = new System.Drawing.Size(86, 28);
+            this.BtnReanudar.TabIndex = 19;
+            this.BtnReanudar.Text = "Reanudar";
+            this.BtnReanudar.UseVisualStyleBackColor = true;
+            this.BtnReanudar.Click += new System.EventHandler(this.BtnReanudar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 405);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.LblContadorClicks);
+            this.Controls.Add(this.BtnNuevaPartida);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Button Btn5;
+        private System.Windows.Forms.Button Btn6;
+        private System.Windows.Forms.Button Btn7;
+        private System.Windows.Forms.Button Btn9;
+        private System.Windows.Forms.Button Btn10;
+        private System.Windows.Forms.Button Btn11;
+        private System.Windows.Forms.Button Btn13;
+        private System.Windows.Forms.Button Btn14;
+        private System.Windows.Forms.Button Btn15;
+        private System.Windows.Forms.Button Btn4;
+        private System.Windows.Forms.Button Btn8;
+        private System.Windows.Forms.Button Btn12;
+        private System.Windows.Forms.Button Btn16;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnNuevaPartida;
+        private System.Windows.Forms.Label LblContadorClicks;
+        private System.Windows.Forms.Timer TimerContador;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnPausar;
+        private System.Windows.Forms.Button BtnReanudar;
+        private System.Windows.Forms.Label LblTiempo;
+    }
+}
+
---
+++ Puzzle/Puzzle/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Clicks = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NuevaPartida();
+        }
+
+        private void Btn16_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn12, Btn16);
+            CambiarBoton(Btn15, Btn16);
+        }        
+
+        private void Btn15_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn11, Btn15);
+            CambiarBoton(Btn14, Btn15);
+            CambiarBoton(Btn16, Btn15);
+        }
+
+        private void Btn14_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn10, Btn14);
+            CambiarBoton(Btn13, Btn14);
+            CambiarBoton(Btn15, Btn14);
+        }
+
+        private void Btn13_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn9, Btn13);
+            CambiarBoton(Btn14, Btn13);
+        }
+
+        private void Btn12_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn8, Btn12);
+            CambiarBoton(Btn11, Btn12);
+            CambiarBoton(Btn16, Btn12);
+        }
+
+        private void Btn11_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn7, Btn11);
+            CambiarBoton(Btn10, Btn11);
+            CambiarBoton(Btn12, Btn11);
+            CambiarBoton(Btn15, Btn11);
+        }
+
+        private void Btn10_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn6, Btn10);
+            CambiarBoton(Btn9, Btn10);
+            CambiarBoton(Btn11, Btn10);
+            CambiarBoton(Btn14, Btn10);
+        }
+
+        private void Btn9_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn5, Btn9);
+            CambiarBoton(Btn10, Btn9);
+            CambiarBoton(Btn13, Btn9);
+        }
+
+        private void Btn8_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn4, Btn8);
+            CambiarBoton(Btn7, Btn8);
+            CambiarBoton(Btn12, Btn8);
+        }
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn3, Btn7);
+            CambiarBoton(Btn6, Btn7);
+            CambiarBoton(Btn8, Btn7);
+            CambiarBoton(Btn11, Btn7);
+        }
+
+        private void Btn6_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn2, Btn6);
+            CambiarBoton(Btn5, Btn6);
+            CambiarBoton(Btn7, Btn6);
+            CambiarBoton(Btn10, Btn6);
+        }
+
+        private void Btn5_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn1, Btn5);
+            CambiarBoton(Btn6, Btn5);
+            CambiarBoton(Btn9, Btn5);
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn3, Btn4);
+            CambiarBoton(Btn5, Btn4);
+            CambiarBoton(Btn8, Btn4);
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn2, Btn3);
+            CambiarBoton(Btn4, Btn3);
+            CambiarBoton(Btn7, Btn3);
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn1, Btn2);
+            CambiarBoton(Btn3, Btn2);
+            CambiarBoton(Btn6, Btn2);
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            CambiarBoton(Btn2, Btn1);
+            CambiarBoton(Btn5, Btn1);            
+        }
+
+        public void NuevaPartida()
+        {
+            var random = new Random();
+            var Numeros = Enumerable.Range(1, 15).OrderBy(x => random.Next()).ToList();
+            var numerosTS = Numeros.Select(x => x.ToString()).ToList();            
+            ReiniciarContador();
+            ReiniciarTimer();
+
+            Btn1.Text = numerosTS[0].ToString();
+            Btn2.Text = numerosTS[1].ToString();
+            Btn3.Text = numerosTS[2].ToString();
+            Btn4.Text = numerosTS[3].ToString();
+            Btn5.Text = numerosTS[4].ToString();
+            Btn6.Text = numerosTS[5].ToString();
+            Btn7.Text = numerosTS[6].ToString();
+            Btn8.Text = numerosTS[7].ToString();
+            Btn9.Text = numerosTS[8].ToString();
+            Btn10.Text = numerosTS[9].ToString();
+            Btn11.Text = numerosTS[10].ToString();
+            Btn12.Text = numerosTS[11].ToString();
+            Btn13.Text = numerosTS[12].ToString();
+            Btn14.Text = numerosTS[13].ToString();
+            Btn15.Text = numerosTS[14].ToString();
+            Btn16.Text = "";
+        }
+
+        private void BtnNuevaPartida_Click(object sender, EventArgs e)
+        {
+            NuevaPartida();
+        }
+
+        public void ReiniciarContador()
+        {
+            LblContadorClicks.Text = "0";
+        }
+
+        public void ReiniciarTimer()
+        {
+            LblTiempo.Text = "00:00:00";
+            Horas = 0;
+            Minutos = 0;
+            Segundos = 0;
+            PararTiempo();
+        }
+
+        public void Comprobar()
+        {
+            if (Btn1.Text == "1" && Btn2.Text == "2" && Btn3.Text == "3" && Btn4.Text == "4" && Btn5.Text == "5" && Btn6.Text == "6" && Btn7.Text == "7" && Btn8.Text == "8" && Btn9.Text == "9" && Btn10.Text == "10" && Btn11.Text == "11" && Btn12.Text == "12" && Btn13.Text == "13" && Btn14.Text == "14" && Btn15.Text == "15" && Btn16.Text == "")
+            {
+
+                MessageBox.Show("¡Has ganado!\n" +
+                    "¡Su tiempo es: "+ LblTiempo + "!\n" +
+                    "¡Realizaste " + Clicks + " movimentos!");
+            }
+        }
+
+        public void ContarClicks()
+        {
+            Clicks++;
+            LblContadorClicks.Text = "Clicks: " + Clicks.ToString();
+        }
+
+        public void CambiarBoton(Button btnVacio, Button btnLleno)
+        {
+            if (btnVacio.Text == "")
+            {
+                btnVacio.Text = btnLleno.Text;
+                btnLleno.Text = string.Empty;
+                ContarClicks();
+                Comprobar();
+                if (Segundos == 0)
+                {
+                    IniciarTiempo();
+                }
+                return;
+            }            
+        }
+
+        double Horas = 0;
+        double Minutos = 0;
+        double Segundos = 0;
+        private void TimerContador_Tick(object sender, EventArgs e)
+        {            
+            Segundos++;
+            LblTiempo.Text = string.Format("{0:00}:{1:00}:{2:00}", Horas, Minutos, Segundos);
+            if (Segundos == 59)
+            {
+                Segundos = 00;
+                Minutos++;
+                LblTiempo.Text = string.Format("{0:00}:{1:00}:{2:00}", Horas, Minutos, Segundos);
+                if (Minutos == 59)
+                {
+                    Minutos = 00;
+                    Horas++;
+                    LblTiempo.Text = string.Format("{0:00}:{1:00}:{2:00}", Horas, Minutos, Segundos);
+                }
+            }
+        }
+
+        private void BtnReanudar_Click(object sender, EventArgs e)
+        {
+            IniciarTiempo();
+        }
+
+        private void BtnPausar_Click(object sender, EventArgs e)
+        {
+            PararTiempo();
+        }
+        public void IniciarTiempo()
+        {
+            TimerContador.Start();
+        }
+
+        public void PararTiempo()
+        {
+            TimerContador.Stop();
+        }
+    }
+}
